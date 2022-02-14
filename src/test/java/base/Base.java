13e4b5b432b11550f415efb9aed04168fc284108
Nafees143 <menafees143@gmail.com>
2022-02-14 17:16:36 +0600

    base class

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -1,5 +1,38 @@
 package base;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
 public class Base {
 
+public static AndroidDriver<AndroidElement> driver;
+	
+	public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException 
+	{
+		//Letting which application to run
+		File f =new File("src");
+		File fs = new File(f,"General-Store.apk");
+		
+		//Opening Emulator
+		DesiredCapabilities cap= new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "");
+		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+		
+		//Running Server in the mentioned port
+		AndroidDriver<AndroidElement> driver= new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		
+		//Adding wait
+		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+		return driver;
+		
+		
+	}
 }
