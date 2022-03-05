f2e7bc53a4443562ec493c10f33c6e08880462ba
Nafees413 <menafees143@gmail.com>
2022-03-05 09:05:15 +0600

    automation build

--- src/test/java/pageObjects/P1_HomePage.java
+++ src/test/java/pageObjects/P1_HomePage.java
@@ -16,4 +16,42 @@ public class P1_HomePage {
 	
 	@AndroidFindBy(id = "com.androidsample.generalstore:id/nameField")
 	public WebElement nameField;
+	
+	@AndroidFindBy(xpath="//*[@text = 'Female']")
+	public WebElement femaleRadio;
+	
+	@AndroidFindBy(xpath="//*[@text = 'Male']")
+	public WebElement maleRadio;
+	
+	@AndroidFindBy(id = "android:id/text1")
+	public WebElement dropDownClick;
+	
+	@AndroidFindBy(xpath ="//*[@text='Bangladesh']")
+	public WebElement countrySelect;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/btnLetsShop")
+	public WebElement letsShop;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/productAddCart")
+	public WebElement addToCart;
+
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/appbar_btn_cart")
+	public WebElement goToCart;
+	
+	@AndroidFindBy(xpath ="//*[@text='Send me e-mails on discounts related to selected products in future']")
+	public WebElement sendMail;
+	
+	@AndroidFindBy(id = "com.androidsample.generalstore:id/appbar_btn_back")
+	public WebElement backButton;
+	
 }
+
+
+
+
+
+
+
+
+
--- src/test/java/testCases/Ecommerce.java
+++ src/test/java/testCases/Ecommerce.java
@@ -2,13 +2,14 @@ package testCases;
 
 import java.io.IOException;
 
+
 import org.testng.annotations.Test;
 
 import base.Base;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import pageObjects.P1_HomePage;
-
+import utilities.Utilities;
 public class Ecommerce extends Base {
 
 	@Test
@@ -19,9 +20,51 @@ public class Ecommerce extends Base {
 		// driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Nafis Khan");
 		
 		P1_HomePage home = new P1_HomePage(driver);
-		home.nameField.sendKeys("Nafis Khan");
+		home.nameField.sendKeys("Ki khobor");
 		Thread.sleep(2000);
-
+		
+		driver.hideKeyboard();
+		
+		home.femaleRadio.click();
+		home.maleRadio.click();
+		Thread.sleep(2000);
+		
+		home.dropDownClick.click();
+		Thread.sleep(2000);
+		
+		Utilities country = new Utilities(driver);
+		country.scrollToText("Bangladesh");
+		Thread.sleep(2000);
+		home.countrySelect.click();
+		
+		home.letsShop.click();
+		Thread.sleep(2000);
+		
+		Utilities product = new Utilities(driver);
+		product.scrollToText("Nike SFB Jungle");
+		Thread.sleep(2000);
+		
+		home.addToCart.click();
+		Thread.sleep(2000);
+		home.goToCart.click();
+		Thread.sleep(2000);
+		home.sendMail.click();
+		Thread.sleep(2000);
+		home.backButton.click();
+		Thread.sleep(2000);
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
